0ec85c5bf5f136ce444ff3ef34c03bdab983f539
tomstagg <tomstagg@gmail.com>
2023-01-20 19:11:30 +0000

    update

--- 08.py
+++ 08.py
@@ -2,10 +2,7 @@
 """Day 8: Treetop Tree House"""
 
 data = open("./resources/08_test.txt").read()
-trees = []
-
-for line in data.split():
-    trees.append([int(i) for i in list(line)])
+trees = [[int(i) for i in list(line)] for line in data.split()]
 
 up = lambda x, y: [t[y] for t in trees[x + 1 :]]
 down = lambda x, y: [t[y] for t in trees[:x]]
@@ -18,18 +15,39 @@ counter = 0
 
 for i in range(len(trees)):
     for j in range(len(trees[0])):
+        # print(trees[i][j])
         if visible(i, j, up) or visible(i, j, down) or visible(i, j, left) or visible(i, j, right):
             counter += 1
-        print(i, j, trees[i][j], visible(i, j, up))
-
 
 print("part 1:", counter)
 
 
-
 # Tests
-# x, y = 4, 0
-# print("down", trees[x][y], down(x, y), visible(x, y, down))
-# print("up", trees[x][y], up(x, y), visible(x, y, up))
-# print("left", trees[x][y], left(x, y), visible(x, y, left))
-# print("right", trees[x][y], right(x, y), visible(x, y, right))
+x, y = 2, 2
+print("down", trees[x][y], down(x, y), visible(x, y, down))
+print("up", trees[x][y], up(x, y), visible(x, y, up))
+print("left", trees[x][y], left(x, y), visible(x, y, left))
+print("right", trees[x][y], right(x, y), visible(x, y, right))
+
+
+from itertools import takewhile
+
+print()
+
+
+for i in takewhile(lambda x: x < 4, [0,1,9,3,4,5,6,7,8,9]):
+    print(i)
+
+# array = [0,1,9,3,4,5,6,7,8,9]
+
+# count = 0
+
+# print([i for i in array if i < 4 break])
+
+# for i in array:
+#     if i < 4:
+#         count += 1
+#     else:
+#         break
+
+# print(count)
